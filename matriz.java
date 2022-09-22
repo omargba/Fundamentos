463a581db33e27f30bb12101df64dd8453d88952
omargba <97587536+omargba@users.noreply.github.com>
2022-09-22 00:04:16 -0700

    Update matriz.java

--- matriz.java
+++ matriz.java
@@ -11,33 +11,32 @@ import java.util.Scanner;
 public class Matriz_tres_cuatro {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Introduce el numero de filas de la matriz a multiplicar");
-        int f1 = teclado.nextInt();
-        System.out.println("Introduce el numero de columnas de la matriz a multiplicar");
-        int c1 = teclado.nextInt();
-        System.out.println("Introduce el numero de filas de la segunda matriz a multiplicar");
-        int f2 = teclado.nextInt();
-        System.out.println("Introduce el numero de columnas de la segunda matriz a multiplicar");
-        int c2 = teclado.nextInt();
-        System.out.println("********Datos ingresados*********");
-        System.out.println("**Matriz 1 de : " + f1 + "x" + c1 +  "**");
-        System.out.println("** Matriz 2 de : " + f2 + "x" + c2 + "**");
+//        System.out.println("Introduce el numero de filas de la matriz a multiplicar");
+//        int f1 = teclado.nextInt();
+//        System.out.println("Introduce el numero de columnas de la matriz a multiplicar");
+//        int c1 = teclado.nextInt();
+//        System.out.println("Introduce el numero de filas de la segunda matriz a multiplicar");
+//        int f2 = teclado.nextInt();
+//        System.out.println("Introduce el numero de columnas de la segunda matriz a multiplicar");
+//        int c2 = teclado.nextInt();
+//        System.out.println("********Datos ingresados*********");
+//        System.out.println("**Matriz 1 de : " + f1 + "x" + c1 +  "**");
+//        System.out.println("** Matriz 2 de : " + f2 + "x" + c2 + "**");
         System.out.println("*****************************");
-        if(c1 == f2){
-            int a[][] = new int[c1][f1];
-            int b[][] = new int [c2][f2];
-            int mR[][] = new int [f1][c2];
+            int a[][] = new int[3][2];
+            int b[][] = new int [2][3];
+            int mR[][] = new int [3][3];
             //Matriz 1
             System.out.println("Ingresa los valores de matriz 1");
-            for (int i = 0; i < f1; i++) {
-                for(int j = 0; j < c1; j++){
+            for (int i = 0; i < 3; i++) {
+                for(int j = 0; j < 2; j++){
                     System.out.println("Introduce los valores del elemento" + i + "," + j + ":");
                     a[i][j] = teclado.nextInt();
                     
                 }
             }
-            for (int i = 0; i < f1; i++) {
-               for(int j = 0; j < c1; j++){
+            for (int i = 0; i < 3; i++) {
+               for(int j = 0; j < 2; j++){
                   System.out.print(  a[i][j] + " " );
 //                    a[i][j] = teclado.nextInt();
     
@@ -46,15 +45,15 @@ public class Matriz_tres_cuatro {
             }
                     //Matriz 2
             System.out.println("Ingresa los valores de matriz 2");
-            for (int i = 0; i < f2; i++) {
-                for(int j = 0; j < c2; j++){
+            for (int i = 0; i < 2; i++) {
+                for(int j = 0; j < 3; j++){
                     System.out.println("Introduce los valores del elemento" + i + "," + j + ":");
                     b[i][j] = teclado.nextInt();
     
                 }
             }
-            for (int i = 0; i < f2; i++) {
-               for(int j = 0; j < c2; j++){
+            for (int i = 0; i < 2; i++) {
+               for(int j = 0; j < 3; j++){
                   System.out.print(  b[i][j] + " " );
 //                    a[i][j] = teclado.nextInt();
     
@@ -65,9 +64,9 @@ public class Matriz_tres_cuatro {
             
             //Matriz Resultante
             System.out.println("Matriz Resultante");
-            for (int i = 0; i < f1; i++) {
-                for (int j = 0; j < c2; j++) {
-                    for (int h = 0; h < c1; h++) {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    for (int h = 0; h < 2; h++) {
                         mR[i][j] += a[i][h] * b[h][j];
                         
                         
@@ -79,14 +78,15 @@ public class Matriz_tres_cuatro {
                 
                 
             }
-        }else{
-            System.out.println("La operacion no se puede realizar");
         }
 
 
             
         }
         
-    }
+    
+    
+
+
     
 
